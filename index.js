0be4822605be8ab0123c349e034346f99bf60e7a
jazzc001 <jazzcheung212@gmail.com>
2022-07-13 16:12:14 +0100

    wrote a few test cases

--- index.js
+++ index.js
@@ -1,5 +1,35 @@
-const index = () => {
+const Rover = (commands) => {
+    let x = 0
+    let y = 0
+    let facing = 'N';
+    let opposition = {  }
 
+    let commandArray = commands.split('');
+
+    const rotateLeft = () => {
+        if (facing === 'W') {
+            facing = 'S'
+        } else if ( facing === 'S') {
+            facing = 'E'
+        } else if (facing === 'E') {
+            facing = 'N'
+        } else {
+            facing = 'W'
+        }
+    }
+
+    commandArray.forEach((i) => {
+
+        if (i === 'M') {
+            y++
+        } else if (i === 'L' ) {
+            rotateLeft();
+        }
+    })
+
+    
+    
+    return `${x}:${y}:${facing}`
 }
 
-module.exports = {index};
+module.exports = {Rover};
--- index.test.js
+++ index.test.js
@@ -1,7 +1,31 @@
-const {} = require('./index')
+const {Rover} = require('./index')
 
-describe('Index', () => {
-    test('should', () => {
-        
+describe('Rover', () => {
+
+    
+    test('starting position should face North', () => {
+        expect(Rover('')).toBe('0:0:N');
     })
+
+    test('move one step', () => {
+        expect(Rover('M')).toBe('0:1:N');
+    })
+
+    test('Turn Left Once', () => {
+        expect(Rover('L')).toBe('0:0:W');
+    })
+
+    test('Turn Left Twice two times', () => {
+        expect(Rover('LL')).toBe('0:0:S');
+    })
+
+    test('Turn Left Three Times three times', () => {
+        expect(Rover('LLL')).toBe('0:0:E');
+    })
+
+    test('Turn Left Four Times', () => {
+        expect(Rover('LLLL')).toBe('0:0:N');
+    })
+
+
 })
